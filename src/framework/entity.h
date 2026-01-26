1f7c345a61912c4f5d43fdc4db9a7ff42eda3c13
Mauro Bruera Mendez <mauro.bruera01@estudiant.upf.edu>
2026-01-26 18:22:05 +0100

    Update

--- src/framework/entity.h
+++ src/framework/entity.h
@@ -1,1 +1,17 @@
-// to be made for lab2
+#pragma once
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+
+#include "framework.h"
+#include "image.h"
+#include "mesh.h"
+#include "camera.h"
+#include "main/includes.h"
+#include "mesh.h"
+
+class Entity {
+    Mesh * mesh = new Mesh();
+    mesh->loadOBJ("meshes/lee.obj");
+};
